bc9cecf8dd7eb4fafcea2eaf3ac5cc62b6f700e7
Your Name <sergio.jn024@gmail.com>
2022-01-08 22:23:25 -0500

    Config

--- Models/Persona.cs
+++ Models/Persona.cs
@@ -5,5 +5,7 @@
         public String? Nombre { get; set; }
         public String? Apellido { get; set; }
         public int Edad { get; set; }
+        public int Dni { get; set; }
+        public string Direccion { get; set; }
     }
 }
