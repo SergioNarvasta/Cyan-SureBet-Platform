cf5763ae3a4457b64857e8d4bd800102cdc20a5f
Sergio Narvasta <sergio.jn024@gmail.com>
2022-07-21 22:13:39 -0500

    Create Models

---
+++ SureBet/Models/Plataforma.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SureBet.Models
+{
+    public class Plataforma
+    {
+        [Key]
+        public int CodPlataforma { get; set; }
+
+        [Display(Name ="Nombre Plataforma")]
+        public string ?NombPlataforma { get; set; }
+    }
+}
