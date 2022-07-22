235b59f8a192ba1d558b55c13ef6722e77a5b421
Sergio Narvasta <sergio.jn024@gmail.com>
2022-07-21 22:21:07 -0500

    Create Model Bet

---
+++ SureBet/Models/ApuestaPrincipal.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SureBet.Models
+{
+    public class ApuestaPrincipal
+    {
+        [Key]
+        public string Id { get; set; }
+        [Required]
+        [StringLength(100)]
+        [Display(Name = "Nombre de Apuesta")]
+        public string ?Descripcion { get; set; }
+
+    }
+}
