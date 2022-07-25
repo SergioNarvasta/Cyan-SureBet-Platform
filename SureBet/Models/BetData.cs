a36379232366a8c2eed3b0cecda507ff71dfe833
Sergio Narvasta <sergio.jn024@gmail.com>
2022-07-24 22:58:35 -0500

    BetData

---
+++ SureBet/Models/BetData.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SureBet.Models
+{
+    public class BetData
+    {
+        [Key]
+       public int Id { get; set; }
+       public string ?Descripcion { get; set; }
+        
+    }
+}
