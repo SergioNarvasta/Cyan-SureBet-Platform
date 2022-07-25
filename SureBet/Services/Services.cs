837a27a3029caea332330128093602cf7570c033
Sergio Narvasta <sergio.jn024@gmail.com>
2022-07-24 23:00:01 -0500

    Services

---
+++ SureBet/Services/Services.cs
@@ -0,0 +1,10 @@
+﻿namespace SureBet.Services
+{
+    public class Services
+    {
+        public static void Register(IServiceCollection services)
+        {
+            services.AddMvc();
+        }   
+    }
+}
