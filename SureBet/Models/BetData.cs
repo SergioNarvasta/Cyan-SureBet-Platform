5106c940f7f64a1f1c2ca4d8e9e057b5496caf36
Sergio Narvasta <sergio.jn024@gmail.com>
2022-07-27 23:55:38 -0500

    Model BetData

--- SureBet/Models/BetData.cs
+++ SureBet/Models/BetData.cs
@@ -6,7 +6,18 @@ namespace SureBet.Models
     {
         [Key]
        public int Id { get; set; }
+       public string ?Evento { get; set; }
+       
        public string ?Descripcion { get; set; }
+
+       public Double Cuota { get; set; }
+
+       public Double Beneficio { get; set; }
+       public string ?CasaApuesta   { get; set; }
+
+       public DateTime Fecha { get; set; }
+
+       public Double Limite { get; set; }
         
     }
 }
