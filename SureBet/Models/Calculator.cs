23a677f903ebb86e889b65325cbe0b6102d7e077
Sergio Narvasta <sergio.jn024@gmail.com>
2022-07-28 00:06:35 -0500

    Calculator

---
+++ SureBet/Models/Calculator.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SureBet.Models
+{
+    public class Calculator
+    {
+        [Key]
+        public int CodCalculator{ get; set; }
+        [Required]
+        [StringLength(100)]
+        [Display(Name = "Nombre de Apuesta")]
+        public string ?Descripcion { get; set; }
+
+        public Double Cuota { get; set; }
+        public Double Probabilidad { get; set; }
+        public Double Apuesta { get; set; }
+
+        public string Moneda { get; set; }
+        public Double Beneficio { get; set; }
+
+    }
+}
