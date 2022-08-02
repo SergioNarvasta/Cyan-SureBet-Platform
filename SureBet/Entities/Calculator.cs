e79ad432578534e12bac68fa8d03ae98147105ff
Sergio Narvasta <sergio.jn024@gmail.com>
2022-08-01 21:22:12 -0500

    public class Calculator

--- SureBet/Entities/Calculator.cs
+++ SureBet/Entities/Calculator.cs
@@ -10,9 +10,12 @@ namespace SureBet.Entities
         [StringLength(100)]
         [Display(Name = "Nombre de Apuesta")]
         public string ?Descripcion { get; set; }
+
+        [StringLength(20)]
         public Double Cuota { get; set; }
         public Double Probabilidad { get; set; }
         public Double Apuesta { get; set; }
+        [Display(Name = "Moneda de Transaccion")]
         public string ?Moneda { get; set; }
         public Double Beneficio { get; set; }
 
