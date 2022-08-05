460bd4d5eea2cd05c0c7847f02b15aed3530125b
Sergio Narvasta <sergio.jn024@gmail.com>
2022-08-04 23:11:21 -0500

    Dev04/sn

---
+++ IS_SureBet/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IS_SureBet.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
