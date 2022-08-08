b3f99fc083832639469e9fb100694239d3b63b4f
Sergio Narvasta <sergio.jn024@gmail.com>
2022-08-08 14:01:08 -0500

    BetData

---
+++ IS_SureBet/Models/BetData.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SureBet.Entities
+{
+    public class BetData
+    {
+        [Key]
+       public int IdBet { get; set; }
+       public string ?Deporte { get; set; }
+       public string ?Evento { get; set; }
+       
+       public string ?Mercado { get; set; }
+
+       public string ?Competicion { get; set; }
+
+       public Double Cuota { get; set; }
+
+       public Double Beneficio { get; set; }
+       public string ?CasaApuesta   { get; set; }
+
+       public DateTime Fecha { get; set; }
+
+       public Double Limite { get; set; }
+
+        
+    }
+}
---
+++ IS_SureBet/Models/Calculator.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SureBet.Entities
+{
+    public class Calculator
+    {
+        [Key]
+        public int IdCalculator{ get; set; }
+        [Required]
+        [StringLength(100)]
+        [Display(Name = "Nombre de Apuesta")]
+        public string ?Descripcion { get; set; }
+
+        [StringLength(20)]
+        public Double Cuota { get; set; }
+        public Double Probabilidad { get; set; }
+        public Double Apuesta { get; set; }
+        [Display(Name = "Moneda de Transaccion")]
+        public string ?Moneda { get; set; }
+        public Double Beneficio { get; set; }
+
+    }
+}
