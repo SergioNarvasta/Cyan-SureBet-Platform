e51cf76e2bf1814536d232e4e0dcff4bdf09c9fd
Sergio Narvasta <sergio.jn024@gmail.com>
2022-08-08 14:16:27 -0500

    DbContext

--- IS_SureBet/Data/ApplicationDbContext.cs
+++ IS_SureBet/Data/ApplicationDbContext.cs
@@ -9,5 +9,9 @@ namespace IS_SureBet.Data
             : base(options)
         {
         }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+          optionsBuilder.UseSqlServer("Server=DESKTOP-2NA2N4M\\SQLEXPRESS;Database=IS_SureBet;Trusted_Connection=true; MultipleActiveResultSets=true;");
+        }
     }
 }
