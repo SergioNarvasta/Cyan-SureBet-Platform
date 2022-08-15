8ee267e867059ec043890b37d8f7f7b03b1195a3
Sergio Narvasta <sergio.jn024@gmail.com>
2022-08-15 16:21:44 -0500

    Delele Files

--- SureBet v 2.0/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SureBet.Entities
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
