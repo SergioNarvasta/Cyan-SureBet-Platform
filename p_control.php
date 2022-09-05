4890db773864732cfc98d2571fe6b2593d2d5aeb
Innovation Services <Sergio.jn024@gmail.com>
2022-09-05 05:35:54 -0500

    control DateTimeZine

--- p_control.php
+++ p_control.php
@@ -10,7 +10,11 @@ $ben=$_POST["Beneficio"];
 $casa=$_POST["Casa"];
 $fev=$_POST["FechaEv"];
 $lim=$_POST["Limite"];
-$frg=getdate();
+//Fecha de Registo Actual en Zona horaria 'America/Lima'
+$Object = new DateTime();
+$Object -> setTimeZone(new DateTimeZone('America/Lima'));
+$frg= $Object -> format('Y-m-d h:i:s a');
+//date('Y-m-d h:i:s a',time());
 
 $sql = "INSERT INTO bet(IdBet, Deporte, Evento, Mercado, Competicion, Cuota, Beneficio, CasaApuesta, FechaEv,Limite,FechaReg) VALUES (NULL,'$dep','$ev','$mer','$comp',$cuo,$ben,'$casa','$fev',$lim,'$frg')";
 //$sql2  ="INSERT INTO `bet` (`IdBet`, `Deporte`, `Evento`, `Mercado`, `Competicion`, `Cuota`, `Beneficio`, `CasaApuesta`, `FechaEv`, `Limite`, `FechaReg`) VALUES (NULL, 'Futbol', 'Alianza vs Universitario', '1x', 'Liga 1 Peru', '1.32', '11', 'BET 365', '2022-08-31', '1900', '2022-08-23')";
