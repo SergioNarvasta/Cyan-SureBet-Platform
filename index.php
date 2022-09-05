7fdfa10e49d1ed394172cc939aabb8c0ff82d852
Sergio Narvasta <snarvasta@hotmail.com>
2022-09-05 15:55:53 -0500

    logos

--- index.php
+++ index.php
@@ -9,7 +9,6 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
    JavaScript Bundle with Popper  -->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
-  
 </head>
 <body> 
   <div id="header">
@@ -67,14 +66,14 @@
           <a href=""><p>Ligas</p><p>Top</p> </a>
           <div class="carrusel">
             <div class="carrusel-items">
+            <div class="carrusel-item">
+                <img width="380px" height="250px" src="img/Ligue1.png" />
+              </div>
               <div class="carrusel-item">
                 <img width="380px" height="250px" src="img/Premier.png" />
               </div>
               <div class="carrusel-item">
                 <img width="380px" height="250px" src="img/Seriea.jpg" />
-              </div>
-              <div class="carrusel-item">
-                <img width="380px" height="250px" src="img/Ligue1.png" />
               </div>
               <div class="carrusel-item">
                 <img width="380px" height="250px" src="img/laliga.png" />
