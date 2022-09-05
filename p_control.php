f2a930c19e29882b18a4fb681d27f355982c0195
Innovation Services <Sergio.jn024@gmail.com>
2022-09-05 06:20:29 -0500

    control Store sn0509

--- p_control.php
+++ p_control.php
@@ -10,15 +10,23 @@ $ben=$_POST["Beneficio"];
 $casa=$_POST["Casa"];
 $fev=$_POST["FechaEv"];
 $lim=$_POST["Limite"];
-//Fecha de Registo Actual en Zona horaria 'America/Lima'
-$Object = new DateTime();
-$Object -> setTimeZone(new DateTimeZone('America/Lima'));
-$frg= $Object -> format('Y-m-d h:i:s a');
-//date('Y-m-d h:i:s a',time());
+$proc=$_POST["Proceso"];
 
-$sql = "INSERT INTO bet(IdBet, Deporte, Evento, Mercado, Competicion, Cuota, Beneficio, CasaApuesta, FechaEv,Limite,FechaReg) VALUES (NULL,'$dep','$ev','$mer','$comp',$cuo,$ben,'$casa','$fev',$lim,'$frg')";
-//$sql2  ="INSERT INTO `bet` (`IdBet`, `Deporte`, `Evento`, `Mercado`, `Competicion`, `Cuota`, `Beneficio`, `CasaApuesta`, `FechaEv`, `Limite`, `FechaReg`) VALUES (NULL, 'Futbol', 'Alianza vs Universitario', '1x', 'Liga 1 Peru', '1.32', '11', 'BET 365', '2022-08-31', '1900', '2022-08-23')";
-$n = mysqli_query($cn,$sql);
+if($proc =1){
+  //Fecha de Registo Actual en Zona horaria 'America/Lima'
+  $Object = new DateTime();
+  $Object -> setTimeZone(new DateTimeZone('America/Lima'));
+  $frg= $Object -> format('Y-m-d h:i:s a');
+  //date('Y-m-d h:i:s a',time());
+
+  $sql = "INSERT INTO bet(IdBet, Deporte, Evento, Mercado, Competicion, Cuota, Beneficio, CasaApuesta, FechaEv,Limite,FechaReg) VALUES (NULL,'$dep','$ev','$mer','$comp',$cuo,$ben,'$casa','$fev',$lim,'$frg')";
+  //$sql2  ="INSERT INTO `bet` (`IdBet`, `Deporte`, `Evento`, `Mercado`, `Competicion`, `Cuota`, `Beneficio`, `CasaApuesta`, `FechaEv`, `Limite`, `FechaReg`) VALUES (NULL, 'Futbol', 'Alianza vs Universitario', '1x', 'Liga 1 Peru', '1.32', '11', 'BET 365', '2022-08-31', '1900', '2022-08-23')";
+  $n = mysqli_query($cn,$sql);
+}else{
+    $sqldel ="Delete from bet  Where FechaEv < CURRENT_DATE()";
+    $n = mysqli_query($cn,$sql);
+}
+
 
 function regresarControl($message) {
     header("location: control.php?error=true&m=$message");
@@ -26,8 +34,8 @@ function regresarControl($message) {
 ?>
 <?php 
   if($n>1){
-    regresarControl('Error en registro, verificar!');
+    regresarControl('Error en proceso, verificar!');
   }else{
-    regresarControl('Registro con exito');
+    regresarControl('Proceso con exito');
   }
 ?>
