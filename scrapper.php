50b0803902d4545d8af3fa92162649d479a21472
Sergio Narvasta <snarvasta@hotmail.com>
2022-09-20 12:43:38 -0500

    Rect file DOM

--- scrapper.php
+++ scrapper.php
@@ -33,55 +33,6 @@
     }else{echo "<br>";echo "---Insert con exito [bet_det] ";echo $res_insert_det1;} 
 
   }catch(Exception $ex){  }
-   //*******************  Datos de 2 ***********************************
-  try {
-    $idcab2=substr(sha1(time()), 0, 16);
-    $lo2   = $content[$i]->innertext;$bus2=strpos($lo2,"UEFA",1);
-    if(($lo2=="España - La Liga")or ($lo2=="Italia - Serie A")or($bus2>1)){
-    $lo2 = $content[$i+2]->innertext;  
-    $vi2 = $content[$i+3]->innertext;  $fe2 = $content[$i+4]->innertext; 
-    $cl2 = $content[$i+5]->innertext;  $ce2 = $content[$i+6]->innertext;  $cv2 = $content[$i+7]->innertext;$i = $i+6;
-    }else{  
-    $vi2 = $content[$i+1]->innertext;  $fe2 = $content[$i+2]->innertext; 
-    $cl2 = $content[$i+3]->innertext;  $ce2 = $content[$i+4]->innertext;  $cv2 = $content[$i+5]->innertext;$i = $i+6;
-    }
-    $insert_cab2 = "INSERT INTO bet_cab(`idcab`,`deporte`,`local`,`visita`,`feceve`,`fecreg`) VALUES('$idcab2','$deporte','$lo2','$vi2','$fe2',CURRENT_TIME)";
-    $res_insert_cab2 = mysqli_query($cn,$insert_cab2);
-    if($res_insert_cab2<1){
-      echo "<br>";echo "Ocurrio un error al Insertar en table [bet_cab]2";echo $res_insert_cab2;
-    }else{echo "<br>";echo "---Insert con exito [bet_cab]2"; echo $lo2.$vi2;echo $res_insert_cab2;} 
-    
-    $insert_det2 = "INSERT INTO bet_det(`idcab`,`iddet`,`casa`,`cuota_local`,`cuota_empate`,`cuota_visita`)VALUES('$idcab2',NULL,'$casa',$cl2,$ce2,$cv2)";
-    $res_insert_det2 = mysqli_query($cn,$insert_det2);
-    if($res_insert_det2<1){
-      echo "<br>";echo "Ocurrio un error al Insertar en table [bet_det]2"; echo "<br>";echo $res_insert_det2;
-    }else{echo "<br>";echo "---Insert con exito [bet_det]2 ";echo $res_insert_det2;} 
-
-  }catch(Exception $ex){  }
-    //*******************  Datos de 3 **********************************
-  try{
-    $lo3   = $content[$i]->innertext;   $idcab3=substr(sha1(time()), 0, 16);
-    if(($lo3=="España - La Liga")or ($lo3=="Italia - Serie A")or($lo3=="UEFA - Champions League")or($lo3="UEFA - Europa League")){
-    $lo3 = $content[$i+4]->innertext;  
-    $vi3 = $content[$i+5]->innertext;  $fe3 = $content[$i+6]->innertext; 
-    $cl3 = $content[$i+7]->innertext;  $ce3 = $content[$i+8]->innertext;  $cv3 = $content[$i+9]->innertext;$i = $i+6;
-    }else{  
-    $vi3 = $content[$i+1]->innertext;  $fe3 = $content[$i+2]->innertext; 
-    $cl3 = $content[$i+3]->innertext;  $ce3 = $content[$i+4]->innertext;  $cv3 = $content[$i+5]->innertext;$i = $i+6;
-    }
-    $insert_cab3 = "INSERT INTO bet_cab(`idcab`,`deporte`,`local`,`visita`,`feceve`,`fecreg`) VALUES('$idcab3','$deporte','$lo3','$vi3','$fe3',CURRENT_TIME)";
-    $res_insert_cab3 = mysqli_query($cn,$insert_cab3);
-    if($res_insert_cab3<1){
-      echo "<br>";echo "Ocurrio un error al Insertar en table [bet_cab]3";echo $res_insert_cab3;
-    }else{echo "<br>";echo "---Insert con exito [bet_cab]3"; echo $lo3.$vi3;echo $res_insert_cab3;} 
-    
-    $insert_det3 = "INSERT INTO bet_det(`idcab`,`iddet`,`casa`,`cuota_local`,`cuota_empate`,`cuota_visita`)VALUES('$idcab3',NULL,'$casa',$cl3,$ce3,$cv3)";
-    $res_insert_det3 = mysqli_query($cn,$insert_det3);
-    if($res_insert_det3<1){
-      echo "<br>";echo "Ocurrio un error al Insertar en table [bet_det]3"; echo "<br>";echo $res_insert_det3;
-    }else{echo "<br>";echo "---Insert con exito [bet_det]3 ";echo $res_insert_det3;} 
-  }catch(Exception $ex){  }  
-   //*******************************************************************
 ?>
 <head>
     <meta charset="UTF-8">
