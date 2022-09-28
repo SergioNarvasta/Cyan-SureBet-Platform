4e66d35a0b2d3cba32e1457dc47974d6be7d32bd
Innovation Services Peru <sergio.narvasta@innservices.com>
2022-09-28 02:13:35 -0500

    Scrapper Pinnacle Finally

--- scrapper.php
+++ scrapper.php
@@ -1,5 +1,6 @@
 <?php
-   //Install purpeteer with -> npm i puppeteer     // Run node Scrapper_Pinnacle.js  //SELECT * FROM bet_cab A left join bet_det B ON A.idcab = B.idcab;
+   //Install purpeteer with -> npm i puppeteer     // Run node Scrapper_Pinnacle.js  
+   //SELECT * FROM bet_cab A left join bet_det B ON A.idcab = B.idcab;
    include ("simple_html_dom.php");      include ("conexion.php") ;
   $file ="Downloads_Web/Pinnacle.html";  $casa ="Pinnacle";
   $filereducido = file_get_contents($file,FALSE,NULL,57761,30800);
@@ -17,33 +18,52 @@
       }
     }
   }
-  function fx_Bet($file,$fin){
+  function fx_Bet($cn,$file,$fin){
     $myhtml = file_get_html($file); $con = 0; $data = $myhtml->find("span"); 
+    $deporte = "Futbol";                  $casa = "Pinnacle";  
     $array = [];  $aux=0;
     foreach($myhtml->find("span") as $content){
       $con++;
+      if($con<$fin){
       $main = $content->innertext;  $type=settype($main,"string"); 
       $buscuota = strpos($main,"Cuota",0); //Si encuentra el flag graba info en variables
       if($buscuota>1){
         array_push($array,$con+4); 
        //echo $con."--Encontrado Guion".$lo;echo "<br>";
-        foreach($myhtml->find("span") as $content){ //for para iterara todos los registros
+        foreach($myhtml->find("span") as $content2){ //for para iterara todos los registros
           $aux++; $nv=$array[0];
           if($aux<$fin){
-            $test = $data[$nv]->innertext;  $busguion = strpos($test,"-",0); 
-            if($busguion>1){
+            $test = $data[$nv]->innertext;  $busguion = strpos($test,"-",0); $filtro = strpos($test,"apuestas",0);
+            $span=htmlentities($test, ENT_QUOTES); $busspan= strpos($span,"span",0);
+            if($busguion>1 and $filtro==false and $busspan==false){
               $array[0] = $nv+2;   $nv=$array[0];
             }
             $lo = $data[$nv]->innertext; $vi = $data[$nv+1]->innertext;  $fe = $data[$nv+2]->innertext;
-            $cl = $data[$nv+3]->innertext; $ce = $data[$nv+4]->innertext;  $cv = $data[$nv+5]->innertext;
-            echo $lo."  -"; echo $vi."  -"; echo $fe; echo "<br>";
-            echo $cl."  -"; echo $ce."  -"; echo $cv; echo "<br>";echo "<br>";
+            $cl=$data[$nv+3]->innertext; $ce = $data[$nv+4]->innertext;  $cv = $data[$nv+5]->innertext;
+            $idcab = substr(substr($lo,0,8).substr($vi,0,8),0,16);
             $array[0] = $nv+6;
+
+            /*$insert_cab = "INSERT INTO bet_cab(`idcab`,`deporte`,`local`,`visita`,`feceve`,`fecreg`)VALUES('$idcab','$deporte','$lo','$vi','$fe',CURRENT_TIME)";
+              $res_insert_cab = mysqli_query($cn,$insert_cab);
+              if($res_insert_cab<1){
+                echo "<br>";echo "--Error [bet_cab]";
+              }else{
+                echo "<br>";echo "--Exito [bet_cab]"; echo $lo.$vi;
+              }
             
+            $insert_det = "INSERT INTO bet_det(`idcab`,`iddet`,`casa`,`cuota_local`,`cuota_empate`,`cuota_visita`,`fecreg`)VALUES('$idcab',NULL,'$casa',$cl,$ce,$cv,CURRENT_TIME)";
+            $res_insert_det = mysqli_query($cn,$insert_det);
+              if($res_insert_det<1){
+                echo "<br>";echo "--Error [bet_det]";
+              }else{
+                echo "<br>";echo "--Exito [bet_det] ";
+              }*/
+            echo $lo."  -"; echo $vi."  -"; echo $fe; echo "<br>";
+            echo $cl."  -"; echo $ce."  -"; echo $cv; echo "<br>";echo "<br>"; 
           }
         }
       } 
-    }
+    }}
   }
 ?>
 <head>
@@ -53,9 +73,9 @@
 <body>
 <center><div class="Info">
   <?php
-     fx_todo($file,153);
+    fx_todo($file,153);
     echo "<br>";echo "<p>---------------------</p>";
-    fx_Bet($file,153);
+    fx_Bet($cn,$file,153);
     echo "<br>";echo "<p>Informacion de Pinnacle</p>";
     
     //echo $filereducido;
