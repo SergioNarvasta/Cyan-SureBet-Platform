4155ae87bd72fa3bc5bfd4ce2b0e57625c207598
Innovation Services Peru <sergio.narvasta@innservices.com>
2022-10-11 20:08:01 -0500

    Project sn1110 bk

--- scrapper_ch.php
+++ scrapper_ch.php
@@ -1,22 +1,33 @@
 <?php
-       include ("simple_html_dom.php");
-
-        $file2 ="Downloads_Web/Chaskibet.html";
-        $filereducido = file_get_contents($file2,FALSE,NULL,1145000,350800);
-        $myhtml = file_get_html($file2);
-        $content = $myhtml->find("span") ; 
-
+  include ("simple_html_dom.php");
+  $file ="Downloads_Web/Chaskibet.html";
+  $filereducido = file_get_contents($file,FALSE,NULL,1145000,350800);
+  $myhtml = file_get_html($file);
+  $content = $myhtml->find("div") ; 
+  function fx_todo($file,$fin){
+    $myhtml = file_get_html($file); $data = $myhtml->find("div");
+    $con = 0;
+     echo "<p>Mostrando toda la Informacion de span  </p>";
+    foreach($myhtml->find("div") as $content){
+      $con++;
+      if($con<$fin){
+        $lo = $data[$con]->innertext;  $type=settype($lo,"string"); 
+        echo $con."-->".$lo;echo "<br>";
+        }
+     }
+  }
 ?>   
 <center>  <div>
 <?php //}  
         echo "<br>";echo "<p>Informacion de ChaskiBet</p>";
-/*
+        //fx_todo($file,200);
+        //echo $filereducido;
+       
         foreach($myhtml->find("tbody") as $content){
               echo $content->innertext;
               echo "<br>";"<br>";   
         }
        //echo $filereducido;
-       //
-    
-    ?>*/
+       
+    ?>
 </div></center>
