670895e98eb11085c95ef30a70a9eaa0377d4fcb
Sergio Narvasta <sergio.jn024@gmail.com>
2023-11-18 15:02:40 -0500

    Create DeleteBetHistory Method in BetHistoryController

--- ITCyan/Cyan.Utils/Common/ResponseModel.cs
+++ ITCyan/Cyan.Utils/Common/ResponseModel.cs
@@ -8,7 +8,9 @@ namespace Cyan.Utils.Common
 {
     public class ResponseModel
     {
-        public string Status {  get; set; }
+        public int Key { get; set; }
+        public bool Status {  get; set; }
         public string Message { get; set; }
+        public string Guid { get; set; }
     }
 }
--- ITCyan/CyanSureBetPlatform/Controllers/BetHistoryController.cs
+++ ITCyan/CyanSureBetPlatform/Controllers/BetHistoryController.cs
@@ -28,29 +28,65 @@ namespace CyanSureBetPlatform.Controllers
         }
 
         [HttpPost]
-        [Route("CreateBetHistory")]
-        public async Task<IActionResult> Create(BetHistory model )
+        [Route("CreateBetHistory", Name = BetHistoryResourcesNames.Routes.PostCreate)]
+        public async Task<IActionResult> Create(BetHistory model)
         {
             if(model == null)
                 BadRequest();
 
             bool result = await _betHistoryAppService.Create(model);
             string message = result ? "Se creo con exito" : "Ocurrio un error";
-            ResponseModel response = new ResponseModel() { Message = message };
+            ResponseModel response = new ResponseModel() 
+            {
+                Message = message,
+                Status= result 
+            };
 
             return Ok(response);
         }
 
         [HttpPost]
-        [Route("GetBetHistoryById")]
+        [Route("GetBetHistoryById", Name = BetHistoryResourcesNames.Routes.GetById)]
         public async Task<IActionResult> GetById(int id)
         {
             if (id == 0)
                 BadRequest();
 
-            var model = await _betHistoryAppService.GetById(id);
-            return Ok(model);
+            BetHistory model = await _betHistoryAppService.GetById(id);
+            var response = model ?? new BetHistory() { };
+            return Ok(response);
         }
 
+        [HttpPut]
+        [Route("UpdateBetHistory", Name = BetHistoryResourcesNames.Routes.PutUpdate)]
+        public async Task<IActionResult> Update(BetHistory model)
+        {
+            if (model == null)
+                BadRequest();
+
+            bool result = await _betHistoryAppService.Update(model);
+            string message = result ? "Se acualizo con exito" : "Ocurrio un error";
+            ResponseModel response = new ResponseModel() 
+            { Message = message, 
+              Status = result,
+              Key = model.BetHistoryId,
+            };
+
+            return Ok(response);
+        }
+
+        [HttpDelete]
+        [Route("DeleteBetHistory", Name = BetHistoryResourcesNames.Routes.Delete)]
+        public async Task<IActionResult> Delete(int id)
+        {
+            if (id == 0)
+                BadRequest();
+
+            bool result = await _betHistoryAppService.Delete(id);
+            string message = result ? "Se elimino con exito" : "Ocurrio un error";
+            ResponseModel response = new ResponseModel() { Message = message };
+
+            return Ok(response);
+        }
     }
 }
