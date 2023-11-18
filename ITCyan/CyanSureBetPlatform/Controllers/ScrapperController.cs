c1afbc59ddb5537083e8fcd8104913feb59d30bb
Sergio Narvasta <sergio.jn024@gmail.com>
2023-11-18 17:13:14 -0500

    Add the ScrapperController

--- ITCyan/CyanSureBetPlatform/Controllers/ScrapperController.cs
+++ ITCyan/CyanSureBetPlatform/Controllers/ScrapperController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Cyan.WebAPI.Constants;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using HtmlAgilityPack;
 
 namespace CyanSureBetPlatform.Controllers
 {
@@ -7,6 +9,23 @@ namespace CyanSureBetPlatform.Controllers
     [ApiController]
     public class ScrapperController : ControllerBase
     {
+        [HttpGet]
+        [Route("GetAllHistory", Name = BetHistoryResourcesNames.Routes.GetQuery)]
+        //[ResponseType(typeof(RepresentationCollectionPaged<PaymentMethodListRp>))]
+        public async Task<IActionResult> GetScraper(int HouseBetId)
+        {
+            // Get the HouseBet By Id 
+            // Get the Url and Scraping the web
+            // Get the data by selectors
+            // Storage the data
+            string HouseBetUrl = "https://www.betsson.com/pe/apuestas-deportivas"; //_houseBetAppService.GetById(HouseBetId);
 
+            var web = new HtmlWeb();
+            // loading the target web page 
+            var document = web.Load(HouseBetUrl);
+
+
+            return Ok();
+        }
     }
 }
